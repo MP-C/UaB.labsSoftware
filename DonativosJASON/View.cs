e946977a89a486c111cb26e1ee5f178caf793aa6
Joni <jpci88@gmail.com>
2022-04-12 21:45:34 +0100

    Teste Joni

--- DonativosJASON/View.cs
+++ DonativosJASON/View.cs
@@ -8,6 +8,7 @@ namespace DonativosJASON
 {
     class View
     {
+        // Teste joni
         private Controller controller; //variaveis da class que apenas se podem usar aqui
         private Model model; //variaveis da class
 
