f08b4616ca0ca5e0e8438f2dc6c053b98321d47f
Goncaloc29 <75841927+Goncaloc29@users.noreply.github.com>
2022-05-05 23:53:46 +0200

    Formulário activo

---
+++ DonativosWindowsForms/DonativosWindowsForms/DonativosWindowsForms/Views/ViewFormulario.cs
@@ -0,0 +1,21 @@
+﻿using DonativosWindowsForms.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonativosWindowsForms.Views
+{
+    public class ViewFormulario
+    {
+        private FormularioDonativo formularioDonativo;
+        public void ActivarInterface()
+        {
+            formularioDonativo = new FormularioDonativo();
+            formularioDonativo.View = this;
+            formularioDonativo.Show();
+        }
+       
+    }
+}
---
+++ DonativosWindowsForms/DonativosWindowsForms/DonativosWindowsForms/Views/ViewMenuIniciar.cs
@@ -0,0 +1,21 @@
+﻿using DonativosWindowsForms.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonativosWindowsForms.Views
+{
+    public class ViewMenuIniciar
+    {
+        private MenuInicial menuInicial;
+        public void ActivarInterface()
+        {
+            menuInicial = new MenuInicial();
+            menuInicial.View = this;
+            menuInicial.Show();
+        }
+
+    }
+}
